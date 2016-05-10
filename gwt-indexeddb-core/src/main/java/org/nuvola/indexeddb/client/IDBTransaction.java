adaabc25438f22186411ad0ba1afc7d1bc186358
Pablo Nussembaum <baunax@gmail.com>
2016-05-10 20:45:06 -0300

    user proper event type

--- gwt-indexeddb-core/src/main/java/org/nuvola/indexeddb/client/IDBTransaction.java
+++ gwt-indexeddb-core/src/main/java/org/nuvola/indexeddb/client/IDBTransaction.java
@@ -8,10 +8,10 @@ public abstract class IDBTransaction extends EventTarget  {
     public Object objectStoreNames;
     public String mode;
     public IDBDatabase db;
-    public Object error;
-    public Object onabort;
-    public Object oncomplete;
-    public Object onerror;
+    public EventHandler<Event> error;
+    public EventHandler<Event> onabort;
+    public EventHandler<Event> oncomplete;
+    public EventHandler<Event> onerror;
     protected IDBTransaction() {}
     public native IDBObjectStore objectStore(String name);
     public native void abort();
