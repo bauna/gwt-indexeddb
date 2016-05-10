7dadb1d35920378ac1db7f378959b483b9e4ade3
Pablo Nussembaum <baunax@gmail.com>
2016-05-10 20:45:20 -0300

    more helper methods

--- gwt-indexeddb-core/src/main/java/org/nuvola/indexeddb/client/JsInteropHelper.java
+++ gwt-indexeddb-core/src/main/java/org/nuvola/indexeddb/client/JsInteropHelper.java
@@ -1,5 +1,7 @@
 package org.nuvola.indexeddb.client;
 
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 
 public class JsInteropHelper {
@@ -28,10 +30,43 @@ public class JsInteropHelper {
      * Useful for casting native implementations to interfaces like JsInterop
      */
     public static native <T> T cast(Object o) /*-{
-      return o;
+        return o;
     }-*/;
 
     public static JSONObject asJSONObject(Object o) {
         return o == null ? new JSONObject() : new JSONObject(cast(o));
     }
+
+    public static JSONArray asJSONArray(Object o) {
+        return o == null ? new JSONArray() : new JSONArray(cast(o));
+    }
+
+    public static native <T> T getProp(Object o, String propertyName) /*-{
+        return o[propertyName];
+    }-*/;
+
+    public static native String getString(Object o, String propertyName) /*-{
+        return "" + o[propertyName];
+    }-*/;
+
+    public static native double getNum(Object o, String propertyName) /*-{
+        return o[propertyName] || 0;
+    }-*/;
+
+    public static native String getISODate(Object o, String propertyName) /*-{
+        var date = o[propertyName] || 0;
+
+        return new Date(date).toISOString();
+    }-*/;
+
+    public static native <T extends JavaScriptObject> T keys(JavaScriptObject object) /*-{
+        var data = [];
+        for (var item in object) {
+          if (Object.prototype.hasOwnProperty.call(object, item)) {
+            var key = @elemental.js.util.JsMapFromStringTo::keyForProperty(Ljava/lang/String;)(item);
+            data.push(key);
+          }
+        }
+        return data;
+    }-*/;
 }
