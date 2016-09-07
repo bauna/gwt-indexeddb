898d44708367b5f8ea0f9643b716fd2fc9c2babb
Pablo Nussembaum <baunax@gmail.com>
2016-09-07 10:08:16 -0300

    add missing @JsProperty

--- gwt-indexeddb-core/src/main/java/org/nuvola/indexeddb/client/IDBIndexOptions.java
+++ gwt-indexeddb-core/src/main/java/org/nuvola/indexeddb/client/IDBIndexOptions.java
@@ -1,12 +1,18 @@
 package org.nuvola.indexeddb.client;
 
 import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 @JsType
 public class IDBIndexOptions {
+    @JsProperty
     public boolean unique;
+
+    @JsProperty
     public boolean multiEntry;
+
+    @JsProperty
     public String locale;
 
     @JsConstructor
