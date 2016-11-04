c5377d63f09ed414e3ef121f17834b41fc5218e1
Pablo Nussembaum <baunax@gmail.com>
2016-11-04 15:05:32 -0300

    add default value

--- gwt-indexeddb-core/src/main/java/org/nuvola/indexeddb/client/IDBCursorDirection.java
+++ gwt-indexeddb-core/src/main/java/org/nuvola/indexeddb/client/IDBCursorDirection.java
@@ -1,6 +1,7 @@
 package org.nuvola.indexeddb.client;
 
 public interface IDBCursorDirection {
+    public static String NEXT = "next";
     public static String NEXTUNIQUE = "nextunique";
     public static String PREV = "prev";
     public static String PREVUNIQUE = "prevunique";
